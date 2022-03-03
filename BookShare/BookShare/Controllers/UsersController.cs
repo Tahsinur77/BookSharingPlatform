08131715c2a450e7bdddbd1588ebcd3fb8ca17e2
tahsinur <tahsinur777@gmail.com>
2022-03-03 15:36:24 +0600

    Database Added with bacpac

---
+++ BookShare/BookShare/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShare.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

279c2e3c8f8b2d94a820d3e36c1519f23f780842
tahsinur <tahsinur777@gmail.com>
2022-03-06 01:12:37 +0600

    admin dash and andmin validation anotation done

---
+++ BookShare/BookShare/Auth/AdminValidation.cs
@@ -0,0 +1,29 @@
+﻿using BookShare.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace BookShare.Auth
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AdminValidation : AuthorizeAttribute
+    {
+
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if(httpContext.Session["user"] != null)
+            {
+                var user = httpContext.Session["user"].ToString();
+                var admin = new JavaScriptSerializer().Deserialize<UserModel>(user);
+                if (admin.Role == "Admin") return true;
+            }
+
+            return false;
+        }
+
+    }
+}

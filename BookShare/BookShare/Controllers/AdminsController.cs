3a0eb5ce93dbc97ebcd9020d59b0e9f91f8b84f8
tahsinur <tahsinur777@gmail.com>
2022-03-06 13:15:41 +0600

    seller list, in seller list document download, admin controller done

--- BookShare/BookShare/Controllers/AdminsController.cs
+++ BookShare/BookShare/Controllers/AdminsController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 using BookShare.Auth;
+using BookShare.Models.Database;
+using BookShare.Models.Entities;
 
 namespace BookShare.Controllers
 {
@@ -14,6 +18,37 @@ namespace BookShare.Controllers
         public ActionResult AdminDash()
         {
             return View();
+        }
+
+        public ActionResult SellerPaindingList()
+        {
+            BookSharingEntities db = new BookSharingEntities();
+            var list = (from x in db.SellerDetails
+                        where x.Status.Equals("painding")
+                        select x).ToList();
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<SellerDetail, SellerDetailsUserModel>());
+            var mapper = new Mapper(config);
+            var sellerDetailsUserModel = mapper.Map<List<SellerDetailsUserModel>>(list);
+
+            return View(sellerDetailsUserModel);
+        }
+
+        public void Download(int id)
+        {
+            BookSharingEntities db = new BookSharingEntities();
+            var fileName = (from x in db.SellerDetails
+                            where x.Id.Equals(id)
+                            select x.ShopDocuments).FirstOrDefault();
+            string FilePath = Server.MapPath("~/Images/"+fileName);
+            WebClient User = new WebClient();
+            Byte[] FileBuffer = User.DownloadData(FilePath);
+            if (FileBuffer != null)
+            {
+                Response.ContentType = "application/pdf";
+                Response.AddHeader("content-length", FileBuffer.Length.ToString());
+                Response.BinaryWrite(FileBuffer);
+            }
         }
     }
 }
---
+++ BookShare/BookShare/Models/Entities/SellerDetailsUserModel.cs
@@ -0,0 +1,20 @@
+﻿using BookShare.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShare.Models.Entities
+{
+    public class SellerDetailsUserModel
+    {
+        public int Id { get; set; }
+        public int SellerId { get; set; }
+        public string Nid { get; set; }
+        public string Status { get; set; }
+        public string ShopNumber { get; set; }
+        public string ShopDocuments { get; set; }
+
+        public  User User { get; set; }
+    }
+}
